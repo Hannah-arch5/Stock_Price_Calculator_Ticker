f291a01f16c94bd990a439357ca9dea1fc928321
Hannah <hannah@Hannahs-Mac-Studio.local>
2026-06-28 03:27:43 +0800

    Change delete icons to X and fix alignment

--- script.js
+++ script.js
@@ -370,9 +370,8 @@ function renderHistory() {
         deleteGroupBtn.title = 'Delete Group';
         deleteGroupBtn.innerHTML = `
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M3 6h18"></path>
-                <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
-                <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
             </svg>
         `;
         deleteGroupBtn.onclick = () => {
